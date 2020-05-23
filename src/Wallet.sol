126b11e78d867f5d636ce763de979a5afc91772d
stackblock <lambdaTL.anthony@gmail.com>
2020-05-23 08:33:49 -0400

    createTranfer(), mapping, and struct

--- src/Wallet.sol
+++ src/Wallet.sol
@@ -3,6 +3,15 @@ pragma solidity 0.6.0;
 contract Wallet {
     address[] public approvers;
     uint public quorum;
+    struct Transfer {
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    mapping(uint => Transfer) public transfers;
+    uint public nextId;
 
     contructor(address[] memory _approvers, uint _quorum) public {
         approvers = _approvers;
@@ -11,5 +20,16 @@ contract Wallet {
 
     function getApprovers() external view returns(address[] memory) {
         return approvers;
+    }
+
+    function createTransfer(uint amount, address payable to) external {
+        transfers[nextId] = Transfer(
+            nextId,
+            amount,
+            to,
+            0,
+            false
+        );
+        nextId ++;
     }
 }
